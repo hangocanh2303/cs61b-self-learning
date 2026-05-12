bd3a552db710ffed0666c8a8621b9121024fecd3
anhhn3 <anhhn3@pvcombank.com.vn>
2026-05-12 14:27:06 +0700

    complete TimeSLList

--- lab3/timingtest/TimeSLList.java
+++ lab3/timingtest/TimeSLList.java
@@ -23,6 +23,44 @@ public class TimeSLList {
 
     public static void timeGetLast() {
         // TODO: YOUR CODE HERE
+        AList<Integer> Ns = createNsList();
+        AList<Double> times = new AList<>();
+        AList<Integer> opCounts = new AList<>();
+
+        int m = 10000;
+
+        for (int i = 0; i < Ns.size(); i++) {
+            SLList<Integer> processSLList = processSLList(Ns.get(i));
+            Stopwatch sp = new Stopwatch();
+            callGetLast(processSLList, m);
+            times.addLast(sp.elapsedTime());
+            opCounts.addLast(m);
+        }
+
+        printTimingTable(Ns, times, opCounts);
+
     }
 
+    private static AList<Integer> createNsList() {
+        AList<Integer> Ns = new AList<>();
+        int n = 1000;
+        while (n <= 128000) {
+            Ns.addLast(n);
+            n *= 2;
+        }
+        return Ns;
+    }
+    private static SLList<Integer> processSLList(int n) {
+        SLList<Integer> Ns = new SLList<>();
+        for (int i = 0; i < n; i++) {
+            Ns.addLast(1);
+        }
+        return Ns;
+    }
+
+    private static void callGetLast(SLList<Integer> slList, int m) {
+        for (int i = 0; i < m; i++) {
+            slList.getLast();
+        }
+    }
 }
