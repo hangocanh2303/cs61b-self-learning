3e72d6f54db237f83f57e96f70d5c15a979af726
anhhn3 <anhhn3@pvcombank.com.vn>
2026-05-12 15:09:10 +0700

    edited TestBuggyAList

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -8,5 +8,38 @@ import static org.junit.Assert.*;
  * Created by hug.
  */
 public class TestBuggyAList {
-  // YOUR TESTS HERE
+    // YOUR TESTS HERE
+    @Test
+    public void testThreeAddThreeRemove() {
+         AListNoResizing<Integer>  aListNoResizing = new AListNoResizing<>();
+         BuggyAList<Integer> buggyAList = new BuggyAList<>();
+
+         for (int i = 4;  i < 7; i++) {
+             aListNoResizing.addLast(i);
+             buggyAList.addLast(i);
+         }
+         assertEquals(aListNoResizing.getLast(), buggyAList.getLast());
+         assertEquals(aListNoResizing.getLast(), buggyAList.getLast());
+         assertEquals(aListNoResizing.getLast(), buggyAList.getLast());
+    }
+
+    @Test
+    public void randomizedTest() {
+        AListNoResizing<Integer> L = new AListNoResizing<>();
+
+        int N = 500;
+        for (int i = 0; i < N; i += 1) {
+            int operationNumber = StdRandom.uniform(0, 2);
+            if (operationNumber == 0) {
+                // addLast
+                int randVal = StdRandom.uniform(0, 100);
+                L.addLast(randVal);
+                System.out.println("addLast(" + randVal + ")");
+            } else if (operationNumber == 1) {
+                // size
+                int size = L.size();
+                System.out.println("size: " + size);
+            }
+        }
+    }
 }
