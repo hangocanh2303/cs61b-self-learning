bbca1a1b3fbeb7882dd36f607a35dbee953a39ca
anhhn3 <anhhn3@pvcombank.com.vn>
2026-05-12 16:20:40 +0700

    edited TestBuggyAList

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -1,5 +1,6 @@
 package randomizedtest;
 
+import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdRandom;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -26,19 +27,33 @@ public class TestBuggyAList {
     @Test
     public void randomizedTest() {
         AListNoResizing<Integer> L = new AListNoResizing<>();
+        BuggyAList<Integer> B = new BuggyAList<>();
 
-        int N = 500;
+        int N = 5000;
         for (int i = 0; i < N; i += 1) {
-            int operationNumber = StdRandom.uniform(0, 2);
+            int operationNumber = StdRandom.uniform(0, 4);
             if (operationNumber == 0) {
                 // addLast
                 int randVal = StdRandom.uniform(0, 100);
                 L.addLast(randVal);
+                B.addLast(randVal);
                 System.out.println("addLast(" + randVal + ")");
             } else if (operationNumber == 1) {
                 // size
-                int size = L.size();
-                System.out.println("size: " + size);
+                int sizeL = L.size();
+                int sizeB = B.size();
+//                System.out.println("size: " + size);
+                assertEquals(sizeL, sizeB);
+            } else if (operationNumber == 2 && L.size() > 0) {
+                int lastL = L.getLast();
+                int lastB = B.getLast();
+//                System.out.println("getLast: " + last);
+                assertEquals(lastL, lastB);
+            } else if (operationNumber == 3 && L.size() > 0) {
+                int lastL = L.removeLast();
+                int lastB = B.removeLast();
+//                System.out.println("removeLast: " + last);
+                assertEquals(lastL, lastB);
             }
         }
     }
