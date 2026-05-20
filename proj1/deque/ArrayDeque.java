14c2d01013b6fbf4ce215ca9b6258286d9d10a4e
anhhn3 <anhhn3@pvcombank.com.vn>
2026-05-20 17:01:15 +0700

    Add comprehensive test suite for `ArrayDeque` and implement core methods: `addFirst`, `addLast`, `isEmpty`, `size`

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -10,33 +10,51 @@ public class ArrayDeque<T> implements Deque<T> {
     public ArrayDeque() {
         items = (T[]) new Object[8];
         nextFirst = 0;
-        nextLast = 0;
+        nextLast = 1;
         size = 0;
     }
 
     @Override
     public void addFirst(T item) {
-
+        items[nextFirst] = item;
+        if (nextFirst == 0) {
+            nextFirst = items.length - 1;
+        } else {
+            nextFirst -= 1;
+        }
+        size += 1;
     }
 
     @Override
     public void addLast(T item) {
-
+        items[nextLast] = item;
+        if (nextLast == items.length - 1) {
+            nextLast = 0;
+        }else {
+            nextLast += 1;
+        }
+        size += 1;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public void printDeque() {
-
+//        for (int i = nextFirst; nextFirst - 1 > 0 && (nextFirst - 1 != nextLast - 1); i -= 1) {
+//            System.out.print(items[i - 1]);
+//        }
+//        for (int i = 0; i < size; i += 1) {
+//            System.out.print(items[i] + " ");
+//        }
+        System.out.println();
     }
 
     @Override
