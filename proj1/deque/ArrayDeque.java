4c4d54204f0babb00d429bdd088ad150ed2bc7e7
anhhn3 <anhhn3@pvcombank.com.vn>
2026-05-20 16:17:28 +0700

    Implement `ArrayDeque` skeleton with core methods

---
+++ proj1/deque/ArrayDeque.java
@@ -0,0 +1,61 @@
+package deque;
+
+public class ArrayDeque<T> implements Deque<T> {
+
+    private T[] items;
+    private int size;
+    private int nextFirst;
+    private int nextLast;
+
+    public ArrayDeque() {
+        items = (T[]) new Object[8];
+        nextFirst = 0;
+        nextLast = 0;
+        size = 0;
+    }
+
+    @Override
+    public void addFirst(T item) {
+
+    }
+
+    @Override
+    public void addLast(T item) {
+
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public void printDeque() {
+
+    }
+
+    @Override
+    public T removeFirst() {
+        return null;
+    }
+
+    @Override
+    public T removeLast() {
+        return null;
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public T getRecursive(int index) {
+        return null;
+    }
+}
