c091c82f86b23be5e2013d543151cffad514e600
anhha <nanhhn@ftech.com.vn>
2026-07-05 08:24:45 +0700

    draft init command

--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -10,15 +10,37 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO: what if args is empty?
+        if (args.length == 0) {
+            Utils.exitWithError("Please enter a command.");
+        }
+
         String firstArg = args[0];
+
         switch(firstArg) {
             case "init":
                 // TODO: handle the `init` command
+                Repository.initCommand();
                 break;
             case "add":
                 // TODO: handle the `add [filename]` command
                 break;
             // TODO: FILL THE REST IN
+        }
+    }
+
+
+    /**
+     * Checks the number of arguments versus the expected number,
+     * throws a RuntimeException if they do not match.
+     *
+     * @param cmd Name of command you are validating
+     * @param args Argument array from command line
+     * @param n Number of expected arguments
+     */
+    public static void validateNumArgs(String cmd, String[] args, int n) {
+        if (args.length != n) {
+            throw new RuntimeException(
+                    String.format("Invalid number of arguments for: %s.", cmd));
         }
     }
 }
