f5c4af8dba8cde1c3baaa22f946d5646be786bfe
anhhn3 <anhhn3@pvcombank.com.vn>
2026-08-20 14:54:47 +0700

    implement Room.java

--- proj3/byow/Core/Room.java
+++ proj3/byow/Core/Room.java
@@ -1,6 +1,7 @@
 package byow.Core;
 
 import byow.TileEngine.TETile;
+import byow.TileEngine.Tileset;
 
 public class Room implements WorldComponent{
     private Position bottomLeft;
@@ -8,15 +9,51 @@ public class Room implements WorldComponent{
     private int height;
 
     public Position getCenter() {
-        return null;
+        return new Position(bottomLeft.getX() + width / 2,
+                bottomLeft.getY() + height / 2);
     }
 
     public boolean overlaps(Room other) {
-        return false;
+        int x1 = bottomLeft.getX();
+        int y1 = bottomLeft.getY();
+        Position otherRoomBottomLeft = other.getBottomLeft();
+        int x2 = otherRoomBottomLeft.getX();
+        int y2 = otherRoomBottomLeft.getY();
+        int w2 = other.getWidth();
+        int h2 = other.getHeight();
+        return (x1 < x2 + w2) &&
+                (x1 + width > x2) &&
+                (y1 < y2 + h2) &&
+                (y1 + height > y2);
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public Position getBottomLeft() {
+        return bottomLeft;
     }
 
     @Override
     public void draw(TETile[][] world) {
-
+        int x = bottomLeft.getX();
+        int y = bottomLeft.getY();
+        int topY = y + height;
+        int topX = x + width;
+        for (int row = y; row < topY; row += 1) {
+            for (int col = x; col < topX; col += 1) {
+                if (row == y || row == topY - 1
+                || col == x || col == topX - 1) {
+                    world[col][row] = Tileset.WALL;
+                } else {
+                    world[col][row] = Tileset.FLOOR;
+                }
+            }
+        }
     }
 }
